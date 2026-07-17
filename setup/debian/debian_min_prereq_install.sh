81cb96aaaef5362f073dbec935f8ea4b30824e87
JohnnyPrimus <lee@partners.biz>
2026-07-17 15:52:39 -0700

    create min install debian prereqs that doesnt require X11

--- setup/debian/debian_min_prereq_install.sh
+++ setup/debian/debian_min_prereq_install.sh
@@ -1,3 +1,3 @@
 sudo apt-get install zsh-common zsh-doc zsh-dev zsh tmux tmuxinator pipenv build-essential git \
 git-lfs fzf thefuck vivid buildd python3-venv python3-pip python3 virtualenv pipenv googler \
-colortail colormake colorized-logs colordiff colorize openjdk-21-jdk npm fonts-powerline
+colortail colormake colorized-logs colordiff colorize fonts-powerline
