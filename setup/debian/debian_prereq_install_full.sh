5d94a9dd15dc6d100d1c31e1a3ebc4a04d719d0c
JohnnyPrimus <lee@partners.biz>
2026-07-17 19:31:46 -0700

    adding gpg support w default git signing config

--- setup/debian/debian_prereq_install_full.sh
+++ setup/debian/debian_prereq_install_full.sh
@@ -1,3 +1,3 @@
 sudo apt-get install --install-suggests zsh-common zsh-doc zsh-dev zsh tmux tmuxinator pipenv build-essential git \
-git-lfs fzf thefuck vivid buildd python3-venv python3-pip python3 virtualenv pipenv googler \
+git-lfs gpg fzf thefuck vivid buildd python3-venv python3-pip python3 virtualenv pipenv googler \
 colortail colormake colorized-logs colordiff colorize colorgcc powerline-fonts npm
