11891280914c6955143ec8ddc1753fd1b95e3ec6
JohnnyPrimus <lee@partners.biz>
2026-07-17 19:50:32 -0700

    removing named file

--- setup/lelando_git.sh
+++
@@ -1,5 +0,0 @@
-git config --global user.email "lee@partners.biz"
-git config --global user.name "JohnnyPrimus"
-git config --global user.signingkey "E665156E25500EF4"
-git config --global commit.gpgsign true
-git config --global tag.gpgsign true
