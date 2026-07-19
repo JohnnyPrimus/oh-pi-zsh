aa2fbe67ad0d412f32d7987816dc7d6d0d5f3708
JohnnyPrimus <lee@partners.biz>
2026-07-19 06:52:16 +0000

    removing extra deb install, add net tools and dns utils to install

--- setup/debian/debian_min_prereq_install.sh
+++ setup/debian/debian_min_prereq_install.sh
@@ -1,3 +1,3 @@
 sudo apt-get install zsh-common zsh-doc zsh-dev zsh tmux tmuxinator pipenv build-essential git \
 git-lfs fzf thefuck vivid buildd python3-venv python3-pip python3 virtualenv pipenv googler \
-colortail gpg colormake colorized-logs colordiff colorize fonts-powerline
+colortail gpg colormake colorized-logs colordiff colorize fonts-powerline net-tools bind9-utils
